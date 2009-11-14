1b358295fb62a39acdfcb5c515380769e11f179d
Jacob <jjcm.linux@gmail.com>
2009-11-14 00:18:27 +0000

    added in score and answers calls in the challenges model

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,3 +1,12 @@
 class Challenge < ActiveRecord::Base
   has_many :submissions
+
+  def lowest_score
+    score = Submission.find(:first, :select=>"score", :order=>"score", :conditions=>{:challenge_id=>self.id})
+    return (score.nil?? "None" : score.score)
+  end
+
+  def number_of_answers
+    answers = Submission.find(:all, :conditions=>{:challenge_id=>self.id}).size
+  end
 end
