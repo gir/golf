bb270c26ad4593e917a611ec7f7f87e53f147f32
gir <kaniel.yano@gmail.com>
2010-02-03 20:11:32 +0000

    Added a title helper and added titles to all the views.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  # Title helper taken from http://railscasts.com/episodes/30-pretty-page-title
+  def title(page_title)
+    content_for(:title) { page_title }
+  end
 end
